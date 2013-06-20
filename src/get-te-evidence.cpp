3bcc0b476e258a064eeb73c46978ce3981cfe505
Matei David <matei@cs.toronto.edu>
2013-06-20 15:27:26 -0400

    update

--- src/get-te-evidence.cpp
+++ src/get-te-evidence.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 
 #include "igzstream.hpp"
+#include "strtk/strtk.hpp"
 #include "globals.hpp"
 #include "Clone.hpp"
 #include "CloneGen.hpp"
@@ -23,19 +24,13 @@ public:
   TSD(int start_, int end_) : start(start_), end(end_), count(0) {}
 };
 
-class MP {
-public:
-  const Pairing * pairing;
-  int t_len;
-  MP(const Pairing * pairing_, int t_len_) : pairing(pairing_), t_len(t_len_) {}
-};
 
 string prog_name;
 vector<TSD> tsd;
-vector<vector<MP> > cluster;
-string (*cnp)(const string&);
-void (*fnp)(const string&, Clone&, int&);
-int taboo_len = 10;
+vector<vector<vector<int>>> cluster;
+string (*cnp)(const string &);
+void (*fnp)(const string &, Clone &, int &);
+int flank_len = 20;
 int min_non_repeat = 20;
 int min_read_len = 20;
 int min_mqv = 0;
@@ -48,174 +43,191 @@ int total_bp_mid;
 
 
 void
-process_mapping_set(const string & s, vector<SamMapping> & v)
+process_mapping_set(const string & clone_name, vector<SamMapping> & v_sm)
 {
-  if ((global::rg_set.rg_list.size() == 0 and v.size() != 1)
-      or (global::rg_set.rg_list.size() > 0 and v.size() != 2)) {
-    cerr << "incorrect number of mappings for clone [" << s << "]" << endl;
-    exit(1);
+  if ((global::rg_set.rg_list.size() == 0 and v_sm.size() != 1)
+      or (global::rg_set.rg_list.size() > 0 and v_sm.size() != 2)) {
+    cerr << "incorrect number of mappings for clone [" << clone_name << "]\n";
+    exit(EXIT_FAILURE);
   }
 
-  vector<Mapping> v2(2);
-  for (size_t j = 0; j < v.size(); ++j) {
-    if (v[j].mapped) v2[j] = convert_SamMapping_to_Mapping(v[j]);
+  // convert to Mapping structures
+  vector<Mapping> v_m(2);
+  for (size_t j = 0; j < v_sm.size(); ++j) {
+    if (v_sm[j].mapped) v_m[j] = convert_SamMapping_to_Mapping(v_sm[j]);
   }
 
+  // trim mapping positions by NM value on either side;
   // if fragment does not capture 20bp outside of the inner repeat, ignore
   if (tsd.size() == 2) {
     long long min_pos = -1;
     long long max_pos = -1;
-    for (size_t j = 0; j < v2.size(); ++j) {
-      if (!v[j].mapped) continue;
+    for (size_t j = 0; j < v_m.size(); ++j) {
+      if (!v_sm[j].mapped) continue;
       int edit_dist = 0;
-      for (size_t i = 0; i < v[j].rest.size(); ++i) {
-	if (v[j].rest[i].key == "NM") {
-	  edit_dist = atoi(v[j].rest[i].value.c_str());
+      for (size_t i = 0; i < v_sm[j].rest.size(); ++i) {
+	if (v_sm[j].rest[i].key == "NM") {
+	  edit_dist = atoi(v_sm[j].rest[i].value.c_str());
 	  break;
 	}
       }
       if (edit_dist > 0) {
-	v2[j].dbPos[0] += edit_dist;
-	v2[j].dbPos[1] -= edit_dist;
+	v_m[j].dbPos[0] += edit_dist;
+	v_m[j].dbPos[1] -= edit_dist;
       }
-      if (min_pos < 0 or v2[j].dbPos[0] < min_pos) min_pos = v2[j].dbPos[0];
-      if (max_pos < 0 or v2[j].dbPos[1] > max_pos) max_pos = v2[j].dbPos[1];
+      if (min_pos < 0 or v_m[j].dbPos[0] < min_pos) min_pos = v_m[j].dbPos[0];
+      if (max_pos < 0 or v_m[j].dbPos[1] > max_pos) max_pos = v_m[j].dbPos[1];
     }
+
     if (min_pos < 0
 	or max_pos < 0
 	or (min_pos > tsd[0].end - min_non_repeat
 	    and max_pos < tsd[1].start + min_non_repeat)) {
       if (global::verbosity > 0) {
-	clog << "fragment [" << v[0].name
+	clog << "fragment [" << v_sm[0].name
 	     << "]: discarding: not enough bp mapped outside of repeat\n";
       }
       return;
     }
   }
 
-  for (size_t j = 0; j < v.size(); ++j) {
-    total_bp += int(v2[j].dbPos[1] - v2[j].dbPos[0] + 1);
-    if (v2[j].dbPos[1] < tsd[0].start - taboo_len) 
-      total_bp_left += int(v2[j].dbPos[1] - v2[j].dbPos[0] + 1);
-    else if (v2[j].dbPos[0] > tsd[tsd.size() - 1].end + taboo_len)
-      total_bp_right += int(v2[j].dbPos[1] - v2[j].dbPos[0] + 1);
-    else if (tsd.size() == 2 and v2[j].dbPos[0] > tsd[0].end + taboo_len
-	and v2[j].dbPos[1] < tsd[1].start - taboo_len)
-      total_bp_mid += int(v2[j].dbPos[1] - v2[j].dbPos[0] + 1);
+  // count bp mapped left/right/between TSDs
+  for (size_t j = 0; j < v_sm.size(); ++j) {
+    if (not v_sm[j].mapped) continue;
+    total_bp += int(v_m[j].dbPos[1] - v_m[j].dbPos[0] + 1);
+    if (v_m[j].dbPos[1] < tsd[0].start - flank_len)
+      total_bp_left += int(v_m[j].dbPos[1] - v_m[j].dbPos[0] + 1);
+    else if (v_m[j].dbPos[0] > tsd[tsd.size() - 1].end + flank_len)
+      total_bp_right += int(v_m[j].dbPos[1] - v_m[j].dbPos[0] + 1);
+    else if (tsd.size() == 2 and v_m[j].dbPos[0] > tsd[0].end + flank_len
+	and v_m[j].dbPos[1] < tsd[1].start - flank_len)
+      total_bp_mid += int(v_m[j].dbPos[1] - v_m[j].dbPos[0] + 1);
   }
 
+  // check if fragment completely captures either TSD
   for (size_t i = 0; i < tsd.size(); ++i) {
-    for (size_t j = 0; j < v.size(); ++j) {
-      if (v[j].mapped
-	  and v[j].mqv >= min_mqv
-	  and v2[j].dbPos[0] <= tsd[i].start - taboo_len
-	  and v2[j].dbPos[1] >= tsd[i].end + taboo_len) {
+    for (size_t j = 0; j < v_sm.size(); ++j) {
+      if (v_sm[j].mapped
+	  and v_sm[j].mqv >= min_mqv
+	  and v_m[j].dbPos[0] <= tsd[i].start - flank_len
+	  and v_m[j].dbPos[1] >= tsd[i].end + flank_len) {
 	// captures this TSD!
 	tsd[i].count++;
 	if (global::verbosity > 0) {
-	  clog << "fragment [" << v[0].name << "]: captures tsd [" << i + 1 << "]\n";
+	  clog << "fragment [" << v_sm[0].name << "]: captures tsd [" << i + 1 << "]\n";
 	}
-	return;
+	//return;
       }
     }
   }
 
-  if (v.size() != 2) {
+  if (v_sm.size() != 2) {
     if (global::verbosity > 0)
-      clog << "fragment [" << v[0].name << "]: discarding: unpaired\n";
+      clog << "fragment [" << v_sm[0].name << "]: discarding: unpaired\n";
     return;
   }
 
-  if (v2[0].dbPos[1] - v2[0].dbPos[0] + 1 < min_read_len
-      or v2[1].dbPos[1] - v2[1].dbPos[0] + 1 < min_read_len) {
+  if (v_m[0].dbPos[1] - v_m[0].dbPos[0] + 1 < min_read_len
+      or v_m[1].dbPos[1] - v_m[1].dbPos[0] + 1 < min_read_len) {
     if (global::verbosity > 0)
-      clog << "fragment [" << v[0].name << "]: discarding: not both long enough\n";
+      clog << "fragment [" << v_sm[0].name << "]: discarding: not both long enough\n";
     return;
   }
 
-  if (not v[0].mapped or not v[1].mapped) {
+  if (not v_sm[0].mapped or not v_sm[1].mapped) {
     if (global::verbosity > 0)
-      clog << "fragment [" << v[0].name << "]: discarding: not both mapped\n";
+      clog << "fragment [" << v_sm[0].name << "]: discarding: not both mapped\n";
     return;
   }
 
-  if (v[0].mqv < min_mqv and v[1].mqv < min_mqv) {
+  if (v_sm[0].mqv < min_mqv and v_sm[1].mqv < min_mqv) {
     if (global::verbosity > 0)
-      clog << "fragment [" << v[0].name << "]: discarding: neither side meets min mqv\n";
+      clog << "fragment [" << v_sm[0].name << "]: discarding: neither side meets min mqv\n";
     return;
   }
 
   const Pairing * pairing = NULL;
+  int rg_idx = -1;
   if (fnp == NULL) {
     // get read group info from SAM tags
     size_t k = 0;
-    while (k < v[0].rest.size() and v[0].rest[k].key != "RG") ++k;
-    if (k >= v[0].rest.size()) {
-      cerr << "could not determine read group for clone: " << s << "\n";
+    while (k < v_sm[0].rest.size() and v_sm[0].rest[k].key != "RG") ++k;
+    if (k >= v_sm[0].rest.size()) {
+      cerr << "could not determine read group for clone: " << clone_name << "\n";
       exit(EXIT_FAILURE);
     }
-    ReadGroup * rg_p = global::rg_set.find_by_name(v[0].rest[k].value);
+    ReadGroup * rg_p = global::rg_set.find_by_name(v_sm[0].rest[k].value);
     if (rg_p == NULL) {
-      cerr << "error: missing read group [" << v[0].rest[k].value
-	   << "] of clone [" << s << "]\n";
-      exit(1);
+      cerr << "error: missing read group [" << v_sm[0].rest[k].value
+	   << "] of clone [" << clone_name << "]\n";
+      exit(EXIT_FAILURE);
     }
     pairing = rg_p->get_pairing();
+    rg_idx = global::rg_set.get_idx(rg_p);
   } else {
     // use full name parser to get read group info
     Clone c;
     int nip;
-    fnp(v[0].name, c, nip);
+    fnp(v_sm[0].name, c, nip);
     pairing = c.pairing;
+
+    // HACK: clone should store read group, not pairing pointer
+    for (size_t i = 0; i < global::rg_set.rg_list.size(); ++i) {
+      if (pairing == &global::rg_set.rg_list[i].pairing) {
+	rg_idx = i;
+	break;
+      }
+    }
+    if (rg_idx < 0) {
+      cerr << "hack failed; pairing pointer not found\n";
+      exit(EXIT_FAILURE);
+    }
   }
 
   // read pair, both mapped, neither straddles a TSD
-  long long left_end = min(v2[0].dbPos[0], v2[1].dbPos[0]);
-  long long right_end = max(v2[0].dbPos[1], v2[1].dbPos[1]);
+  long long left_end = min(v_m[0].dbPos[0], v_m[1].dbPos[0]);
+  long long right_end = max(v_m[0].dbPos[1], v_m[1].dbPos[1]);
 
   if (tsd.size() == 1) {
-    if (left_end < tsd[0].start - taboo_len
-	and right_end > tsd[0].end + taboo_len) {
+    if (left_end <= tsd[0].start - flank_len
+	and right_end >= tsd[0].end + flank_len) {
       if (global::verbosity > 0) {
-	clog << "fragment [" << v[0].name << "]: straddles single tsd\n";
+	clog << "fragment [" << v_sm[0].name << "]: straddles single tsd\n";
       }
-      cluster[0].push_back(MP(pairing, pairing->get_t_len(v2[0], 0, v2[1], 0)));
+      cluster[0][rg_idx].push_back(pairing->get_t_len(v_m[0], 0, v_m[1], 0));
     }
   } else {
-    if (left_end < tsd[0].start - taboo_len and
-	right_end > tsd[1].end + taboo_len) {
+    if (left_end <= tsd[0].start - flank_len and
+	right_end >= tsd[1].end + flank_len) {
       if (global::verbosity > 0) {
-	clog << "fragment [" << v[0].name << "]: straddles both tsds\n";
+	clog << "fragment [" << v_sm[0].name << "]: straddles both tsds\n";
       }
-      cluster[0].push_back(MP(pairing, pairing->get_t_len(v2[0], 0, v2[1], 0)));
-    } else if (left_end < tsd[0].start - taboo_len and
-	       right_end > tsd[0].end + taboo_len and
-	       right_end < tsd[1].start - taboo_len) {
+      cluster[0][rg_idx].push_back(pairing->get_t_len(v_m[0], 0, v_m[1], 0));
+    } else if (left_end <= tsd[0].start - flank_len and
+	       right_end >= tsd[0].end + flank_len and
+	       right_end <= tsd[1].start - flank_len) {
       if (global::verbosity > 0) {
-	clog << "fragment [" << v[0].name << "]: straddles tsd [1]\n";
+	clog << "fragment [" << v_sm[0].name << "]: straddles tsd [1]\n";
       }
-      cluster[1].push_back(MP(pairing, pairing->get_t_len(v2[0], 0, v2[1], 0)));
-    } else if (left_end > tsd[0].end + taboo_len and
-	       left_end < tsd[1].start - taboo_len and
-	       right_end > tsd[1].end + taboo_len) {
+      cluster[1][rg_idx].push_back(pairing->get_t_len(v_m[0], 0, v_m[1], 0));
+    } else if (left_end >= tsd[0].end + flank_len and
+	       left_end <= tsd[1].start - flank_len and
+	       right_end >= tsd[1].end + flank_len) {
       if (global::verbosity > 0) {
-	clog << "fragment [" << v[0].name << "]: straddles tsd [2]\n";
+	clog << "fragment [" << v_sm[0].name << "]: straddles tsd [2]\n";
       }
-      cluster[2].push_back(MP(pairing, pairing->get_t_len(v2[0], 0, v2[1], 0)));
+      cluster[2][rg_idx].push_back(pairing->get_t_len(v_m[0], 0, v_m[1], 0));
     }
   }
 }
 
 void
-print_cluster_evidence(const vector<MP>& v, ostream& os)
+print_cluster_evidence(const vector<vector<int>> & v, ostream & os)
 {
-  if (v.size() == 0) {
-    os << ".";
-  } else {
-    for (size_t i = 0; i < v.size(); ++i) {
-      if (i > 0) os << ";";
-      os << v[i].pairing->mean << "," << v[i].pairing->stddev << "," << v[i].t_len;
-    }
+  for (size_t i = 0; i < v.size(); ++i) {
+    if (i > 0) os << ";";
+    os << global::rg_set.rg_list[i].get_num_id() << ":"
+       << strtk::join(",", v[i]);
   }
 }
 
@@ -320,9 +332,11 @@ main(int argc, char* argv[])
 
   //if (global::verbosity > 0) clog << "number of threads: " << num_threads << '\n';
   if (tsd.size() == 1) {
-    cluster = vector<vector<MP> >(1);
+    cluster = vector<vector<vector<int>>>
+      (1, vector<vector<int>>(global::rg_set.rg_list.size()));
   } else if (tsd.size() == 2) {
-    cluster = vector<vector<MP> >(3);
+    cluster = vector<vector<vector<int>>>
+      (3, vector<vector<int>>(global::rg_set.rg_list.size()));
   } else {
     cerr << "wrong number of tsds\n";
     usage(cerr);
@@ -380,19 +394,19 @@ main(int argc, char* argv[])
     print_cluster_evidence(cluster[2], cout);
   }
   cout << "\t"
-       << (reg_start < tsd[0].start - taboo_len ?
-	   double(total_bp_left) / double (tsd[0].start - taboo_len - reg_start + 1)
+       << (reg_start < tsd[0].start - flank_len ?
+	   double(total_bp_left) / double (tsd[0].start - flank_len - reg_start + 1)
 	   : 0)
        << "\t"
-       << (reg_end > tsd[tsd.size() - 1].end + taboo_len ?
+       << (reg_end > tsd[tsd.size() - 1].end + flank_len ?
 	   double(total_bp_right)
-	   / double (reg_end - tsd[tsd.size() - 1].end - taboo_len + 1)
+	   / double (reg_end - tsd[tsd.size() - 1].end - flank_len + 1)
 	   : 0);
   if (tsd.size() == 2) {
     cout << "\t"
-	 << (tsd[0].end + taboo_len < tsd[1].end - taboo_len ?
+	 << (tsd[0].end + flank_len < tsd[1].end - flank_len ?
 	     double(total_bp_mid)
-	     / double (tsd[1].end - taboo_len - tsd[0].end - taboo_len + 1)
+	     / double (tsd[1].end - flank_len - tsd[0].end - flank_len + 1)
 	     : 0);
   }
   cout << "\n";
